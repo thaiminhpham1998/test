9eeccfb6243433002eb83222d8257f8d99ea5db8
“MinhThai” <thaisinh1181998@gmail.com>
2024-02-22 12:36:48 +0700

    test commit 2

---
+++ server/odoo/addons/estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'website': 'https://www.odoo.com/app/estate',
+    'author': "Author Name",
+    'category': 'Marketing/Estate',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
